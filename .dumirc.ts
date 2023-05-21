a274000fecc44f4a56c01b78bc10cb9eee703390
arvinxx <arvinx@foxmail.com>
2023-05-21 15:25:03 +0800

    :memo: docs: 更新文档配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from 'dumi';
 import { homepage } from './package.json';
 
-export default defineConfig({
-  themeConfig: {
-    name: 'Lobe UI',
-    socialLinks: {
-      github: homepage,
-    },
+const isProd = process.env.NODE_ENV === 'production';
+
+const themeConfig = {
+  name: 'Lobe UI',
+  description: 'Empowering your AI dreams with LobeHub',
+  logo: 'https://raw.githubusercontent.com/lobehub/.github/main/profile/Logo.webp',
+  footer: 'LobeHub',
+  socialLinks: {
+    github: homepage,
   },
+};
+
+export default defineConfig({
+  themeConfig,
+  title: 'LobeHub UI Kit',
+  favicons: ['https://raw.githubusercontent.com/lobehub/favicon/main/dist/favicon.ico'],
+  npmClient: 'pnpm',
+  base: '/',
+  publicPath: '/',
+  ssr: isProd ? {} : false,
+  apiParser: isProd ? {} : false,
+  resolve: isProd
+    ? {
+        entryFile: './src/index.ts',
+      }
+    : {},
+  define: {
+    'process.env': process.env,
+  },
+  extraBabelPlugins: ['babel-plugin-styled-components'],
 });
