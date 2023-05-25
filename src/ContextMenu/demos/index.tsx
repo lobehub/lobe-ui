c5b864c341d7b9753d7a59b95936351fdb633845
arvinxx <arvinx@foxmail.com>
2023-05-25 19:58:36 +0800

    :memo: docs: update demo

--- src/ContextMenu/demos/index.tsx
+++ src/ContextMenu/demos/index.tsx
@@ -1,4 +1,6 @@
-import { ContextMenu } from '@lobehub/ui';
+import { ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons';
+import { ContextMenu, Icon } from '@lobehub/ui';
+import { BoxSelectIcon, CopyIcon } from 'lucide-react';
 
 export default () => {
   return (
@@ -6,9 +8,28 @@ export default () => {
       <div>RightClick</div>
       <ContextMenu
         items={[
-          { label: '1', key: '1' },
-          { label: '2', key: '2' },
-          { label: '3', key: '3' },
+          {
+            key: 'copy',
+            label: '复制',
+            icon: <Icon size={'small'} icon={CopyIcon} />,
+            shortcut: ['meta', 'C'],
+          },
+          {
+            key: 'selectAll',
+            label: '选择全部',
+            icon: <Icon size={'small'} icon={BoxSelectIcon} />,
+            shortcut: ['meta', 'A'],
+          },
+          { label: '放大', key: 'zoomIn', icon: <ZoomInOutlined /> },
+          { label: '缩小', key: 'zoomOut', icon: <ZoomOutOutlined /> },
+          {
+            label: '最近打开的文件...',
+            key: 'recent',
+            children: [
+              { key: '1', label: '文件1' },
+              { key: '2', label: '文件2' },
+            ],
+          },
         ]}
       />
     </>
