131c7c8c1b6f263090cec759af22aa1813b605f9
CanisMinor <i@canisminor.cc>
2023-05-26 00:17:51 +0800

    🛠️ chore: Update base.ts

--- src/styles/theme/base.ts
+++ src/styles/theme/base.ts
@@ -3,7 +3,7 @@ import { ThemeConfig } from 'antd';
 const FONT_EMOJI = `"Segoe UI Emoji", "Segoe UI Symbol", "Apple Color Emoji", "Twemoji Mozilla", "Noto Color Emoji", "Android Emoji"`;
 const FONT_EN = `"HarmonyOS Sans", "Segoe UI", "SF Pro Display",-apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`;
 const FONT_CN = `"HarmonyOS Sans SC", "PingFang SC", "Hiragino Sans GB", "Microsoft Yahei UI", "Microsoft Yahei", "Source Han Sans CN", sans-serif`;
-const FONT_CODE = `ui-monospace, Hack, "SFMono Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace`;
+const FONT_CODE = `Hack, "SFMono Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace`;
 export const baseTheme: ThemeConfig = {
   token: {
     colorInfo: '#0070f3',
