7fb62c5c2803b034e01920ea0cd2c7f6226fbf63
canisminor1990 <i@canisminor.cc>
2023-08-11 16:57:28 +0800

    ✨ feat: Add @react-spring/three dependency and implement hover state and rotation in Logo.tsx
    This commit introduces a new dependency "@react-spring/three" in the package.json file. In the Logo.tsx file, imports and code related to the "@react-spring/three" dependency have been added. These changes include adding a hover state, handling events like pointer over and pointer out, and applying rotation to the logo.
    
    Changes:
    - Add @react-spring/three dependency in package.json
    - Implement hover state and rotation in Logo.tsx

--- src/LogoThree/Logo.tsx
+++ src/LogoThree/Logo.tsx
@@ -1,6 +1,7 @@
+import { a, config, useSpring } from '@react-spring/three';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { memo, useRef } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 import type { Group } from 'three';
 
 const NUM = 10;
@@ -11,6 +12,18 @@ export interface LogoProps {
 const Logo = memo<LogoProps>(({ ...props }) => {
   const ref: any = useRef<Group>(null);
   const { scene } = useGLTF('https://gw.alipayobjects.com/os/kitchen/ygwdCZyaMZ/lobehub.gltf');
+  // Hover state
+  const [hovered, setHover] = useState(false);
+  useEffect(() => void (document.body.style.cursor = hovered ? 'pointer' : 'auto'), [hovered]);
+  // Events
+  const [toggle, set] = useState(0);
+  const [{ x }] = useSpring({ config: config.wobbly, x: toggle }, [toggle]);
+  const onPointerOver = useCallback(() => setHover(true), []);
+  const onPointerOut = useCallback(() => setHover(false), []);
+  const onClick = useCallback(() => set((toggle) => Number(!toggle)), [set]);
+
+  const rotation = x.to([0, 1], [0, Math.PI * 2]);
+
   useFrame((state) => {
     const t = state.clock.getElapsedTime();
     ref.current.rotation.set(
@@ -22,8 +35,15 @@ const Logo = memo<LogoProps>(({ ...props }) => {
   });
   return (
     <group ref={ref}>
-      {/* eslint-disable-next-line react/no-unknown-property */}
-      <primitive object={scene} {...props} />
+      <a.group
+        onClick={onClick}
+        onPointerOut={onPointerOut}
+        onPointerOver={onPointerOver}
+        rotation-y={rotation}
+      >
+        {/* eslint-disable-next-line react/no-unknown-property */}
+        <primitive object={scene} {...props} />
+      </a.group>
     </group>
   );
 });
