1e820669d6ee895d733ae32fbac1e8ccc205580d
CanisMinor <i@canisminor.cc>
2023-09-05 04:05:21 +0000

    🐛 fix: Fix markdown code style

--- src/Markdown/Code.tsx
+++ src/Markdown/Code.tsx
@@ -5,7 +5,7 @@ import { useStyles } from '@/Markdown/style';
 const Code = memo((p: PropsWithChildren<any>) => {
   const { styles } = useStyles();
 
-  return <code className={styles.code}>`{p.children}`</code>;
+  return <code className={styles.code}>{p.children}</code>;
 });
 
 export default Code;
