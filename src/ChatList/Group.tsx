a1128d676cfcbe8862585c44eacfc605d8258fe7
canisminor1990 <i@canisminor.cc>
2023-09-07 18:04:00 +0800

    ✨ feat: Replace 'Nav' component with 'GroupNav' component
    The 'Nav' component has been replaced with the 'GroupNav' component in the 'Item' component. This change involves removing the import statement for the 'antd-style' and 'react-layout-kit' libraries and importing the 'GroupNav' component instead. The 'GroupNav' component now renders a list of avatars based on the 'data' prop, with the active avatar highlighted.
    
    Changes:
    - Remove import statement for 'antd-style' and 'react-layout-kit'
    - Import 'GroupNav' component
    - Replace 'Nav' component with 'GroupNav' component in 'Item' component

--- src/ChatList/Group.tsx
+++ src/ChatList/Group.tsx
@@ -1,9 +1,6 @@
-import { useTheme } from 'antd-style';
-import { memo, useCallback, useMemo, useState } from 'react';
-import { Flexbox } from 'react-layout-kit';
+import { memo, useMemo, useState } from 'react';
 
-import Avatar from '@/Avatar';
-
+import GroupNav from './GroupNav';
 import Item, { ChatListItemProps } from './Item';
 
 interface ChatListGroup {
@@ -13,56 +10,15 @@ interface ChatListGroup {
 
 const Group = memo<ChatListGroup>(({ data, meta }) => {
   const [active, setActive] = useState(data[0].id);
-  const theme = useTheme();
+
   const chatItem = useMemo(
     () => data.find((item) => item.id === active) || data[0],
     [data, active],
   );
 
-  const Nav = useCallback(() => {
-    let count = 1;
-    return data.map((item) => {
-      let avatar: string | undefined;
-      const isAvtive = active === item.id;
-      const metaAvatar = item?.meta?.avatar;
-      switch (item.role) {
-        case 'assistant': {
-          avatar = String(count);
-          break;
-        }
-        case 'function': {
-          avatar = metaAvatar || '🧩';
-          break;
-        }
-        case 'system': {
-          avatar = metaAvatar || '🚨';
-          break;
-        }
-        default: {
-          avatar = metaAvatar || String(count);
-          break;
-        }
-      }
-      count++;
-      return (
-        <Avatar
-          avatar={avatar}
-          background={isAvtive ? theme.colorPrimary : theme.colorBgElevated}
-          key={item.id}
-          onClick={() => setActive(item.id)}
-          size={20}
-        />
-      );
-    });
-  }, [active, data]);
-
   return (
     <Item
-      groupNav={
-        <Flexbox align={'center'} gap={4}>
-          <Nav />
-        </Flexbox>
-      }
+      groupNav={<GroupNav active={active} data={data} setActive={setActive} />}
       {...chatItem}
       meta={meta}
     />
