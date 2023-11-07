02e67b76673e02be5e07829695bdead2bcad6e4f
arvinxx <arvinx@foxmail.com>
2023-11-07 09:58:28 +0800

    :fire: fix: remove tts temporally to fix app build

--- src/useTTS/utils/genSendContent.ts
+++
@@ -1,8 +0,0 @@
-export const genSendContent = (header: { [key: string]: string }, data: string) => {
-  const content = [];
-  for (const [key, value] of Object.entries(header)) {
-    content.push(`${key}:${value}`);
-  }
-  content.push('', data);
-  return content.join('\r\n');
-};
