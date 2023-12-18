1ba629103a4a149ad7872815fc4c68aa971a1c0d
wuxh <wxh1220@gmail.com>
2023-12-18 16:07:09 +0800

    :bug: fix: DraggablePanelHeader children not working

--- src/DraggablePanel/components/DraggablePanelHeader.tsx
+++ src/DraggablePanel/components/DraggablePanelHeader.tsx
@@ -15,38 +15,46 @@ export interface DraggablePanelHeaderProps extends DivProps {
   title: string;
 }
 
-const DraggablePanelHeader = memo<DraggablePanelHeaderProps>(
-  ({ pin, setPin, className, setExpand, title, position = 'left', ...rest }) => {
-    const { cx, styles } = useStyles();
+const DraggablePanelHeader = memo<DraggablePanelHeaderProps>((props) => {
+  const {
+    pin,
+    setPin,
+    className,
+    setExpand,
+    title,
+    position = 'left',
+    children = title,
+    ...rest
+  } = props;
+  const { cx, styles } = useStyles();
 
-    const [isPinned, setIsPinned] = useControlledState(false, {
-      onChange: setPin,
-      value: pin,
-    });
+  const [isPinned, setIsPinned] = useControlledState(false, {
+    onChange: setPin,
+    value: pin,
+  });
 
-    const panelIcon = (
-      <ActionIcon
-        icon={PanelLeft}
-        onClick={() => setExpand?.(false)}
-        size={{ blockSize: 24, fontSize: 14 }}
-      />
-    );
-    const pinIcon = (
-      <ActionIcon
-        active={pin}
-        icon={pin ? Pin : PinOff}
-        onClick={() => setIsPinned(!isPinned)}
-        size={{ blockSize: 24, fontSize: 14 }}
-      />
-    );
-    return (
-      <div className={cx(styles.header, className)} {...rest}>
-        {position === 'left' ? panelIcon : pinIcon}
-        {title}
-        {position === 'left' ? pinIcon : panelIcon}
-      </div>
-    );
-  },
-);
+  const panelIcon = (
+    <ActionIcon
+      icon={PanelLeft}
+      onClick={() => setExpand?.(false)}
+      size={{ blockSize: 24, fontSize: 14 }}
+    />
+  );
+  const pinIcon = (
+    <ActionIcon
+      active={pin}
+      icon={pin ? Pin : PinOff}
+      onClick={() => setIsPinned(!isPinned)}
+      size={{ blockSize: 24, fontSize: 14 }}
+    />
+  );
+  return (
+    <div className={cx(styles.header, className)} {...rest}>
+      {position === 'left' ? panelIcon : pinIcon}
+      {children}
+      {position === 'left' ? pinIcon : panelIcon}
+    </div>
+  );
+});
 
 export default DraggablePanelHeader;
