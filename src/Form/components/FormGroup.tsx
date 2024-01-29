20acfdc39c57b7c5d61fec3e1f29ddcd576fdaa2
Arvin Xu <arvinx@foxmail.com>
2024-01-29 05:50:53 +0000

    :bug: fix: revert to fix

--- src/Form/components/FormGroup.tsx
+++ src/Form/components/FormGroup.tsx
@@ -145,7 +145,7 @@ export interface FormGroupProps extends CollapseProps {
   children: ReactNode;
   defaultActive?: boolean;
   extra?: ReactNode;
-  icon?: IconProps['icon'] | ReactNode;
+  icon?: IconProps['icon'];
   itemsType?: ItemsType;
   title?: string;
   variant?: FormVariant;
@@ -168,7 +168,7 @@ const FormGroup = memo<FormGroupProps>(
 
     const titleContent = (
       <Flexbox className={styles.title} gap={8} horizontal>
-        {icon && typeof icon === 'function' ? <Icon icon={icon} /> : icon}
+        {icon && <Icon icon={icon} />}
         {title}
       </Flexbox>
     );
