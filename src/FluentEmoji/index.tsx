2fa3acc052571cc4021be4b3c71ac3a14f3cf423
tnga <devtnga@gmail.com>
2024-02-04 17:57:02 +0100

    ♻️ refactor: Update version to 'latest' for vendor assets
    Update the version of all vendor assets to 'latest' in files.
    This refactor aims to simplify the version management process for vendor assets like logos, making it easier to maintain and update them in the future.

--- src/FluentEmoji/index.tsx
+++ src/FluentEmoji/index.tsx
@@ -50,14 +50,14 @@ const FluentEmoji = memo<FluentEmojiProps>(
           return genCdnUrl({
             path: `assets/${emojiName}.webp`,
             pkg: '@lobehub/assets-emoji-anim',
-            version: '1.0.0',
+            version: 'latest',
           });
         }
         case '3d': {
           return genCdnUrl({
             path: `assets/${emojiName}.webp`,
             pkg: '@lobehub/assets-emoji',
-            version: '1.3.0',
+            version: 'latest',
           });
         }
       }
--- src/Logo/style.ts
+++ src/Logo/style.ts
@@ -3,22 +3,22 @@ import { createStyles } from 'antd-style';
 export const LOGO_3D = {
   path: 'assets/logo-3d.webp',
   pkg: '@lobehub/assets-logo',
-  version: '1.1.0',
+  version: 'latest',
 };
 export const LOGO_FLAT = {
   path: 'assets/logo-flat.svg',
   pkg: '@lobehub/assets-logo',
-  version: '1.1.0',
+  version: 'latest',
 };
 export const LOGO_TEXT = {
   path: 'assets/logo-text.svg',
   pkg: '@lobehub/assets-logo',
-  version: '1.1.0',
+  version: 'latest',
 };
 export const LOGO_HIGH_CONTRAST = {
   path: 'assets/logo-high-contrast.svg',
   pkg: '@lobehub/assets-logo',
-  version: '1.1.0',
+  version: 'latest',
 };
 
 export const useStyles = createStyles(({ css }) => {
