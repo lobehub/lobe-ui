d74d31bdfc6a8794b1cba958295d001baa40bf8b
wuxh <wxh1220@gmail.com>
2024-02-07 15:37:56 +0800

    :sparkles: chore(lint): add css logical properties rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,8 @@
-const config = require('@lobehub/lint').stylelint;
-
 module.exports = {
-  ...config,
+  extends: require('@lobehub/lint').stylelint,
+  plugins: ['stylelint-use-logical-spec'],
   rules: {
+    'liberty/use-logical-spec': ['always', { except: ['float', /^((min|max)-)?(height|width)$/i] }],
     'custom-property-pattern': null,
     'selector-pseudo-element-no-unknown': null,
     'no-descending-specificity': null,
