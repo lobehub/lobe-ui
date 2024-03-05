c1900b81ba2d7e098db1c47d07cb2d87e30213c8
Arvin Xu <arvinx@foxmail.com>
2024-03-05 06:57:04 +0000

    :rewind: fix: Revert "✨ feat: Use ReactSVG to load svg logos"
    This reverts commit 1d0b79fa

--- src/Logo/style.ts
+++ src/Logo/style.ts
@@ -3,22 +3,12 @@ import { createStyles } from 'antd-style';
 export const LOGO_3D = {
   path: 'assets/logo-3d.webp',
   pkg: '@lobehub/assets-logo',
-  version: 'latest',
+  version: '1.1.0',
 };
 export const LOGO_FLAT = {
   path: 'assets/logo-flat.svg',
   pkg: '@lobehub/assets-logo',
-  version: 'latest',
-};
-export const LOGO_TEXT = {
-  path: 'assets/logo-text.svg',
-  pkg: '@lobehub/assets-logo',
-  version: 'latest',
-};
-export const LOGO_HIGH_CONTRAST = {
-  path: 'assets/logo-high-contrast.svg',
-  pkg: '@lobehub/assets-logo',
-  version: 'latest',
+  version: '1.1.0',
 };
 
 export const useStyles = createStyles(({ css }) => {
