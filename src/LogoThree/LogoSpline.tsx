225ebab59d91e98605e77d9041fee119f3b1a3ad
CanisMinor <i@canisminor.cc>
2024-03-28 15:15:49 +0800

    🐛 fix: Fix LogoSpline Props

--- src/LogoThree/LogoSpline.tsx
+++ src/LogoThree/LogoSpline.tsx
@@ -1,60 +1,38 @@
 import Spline, { type SplineProps } from '@splinetool/react-spline';
 import { useThemeMode } from 'antd-style';
 import { CSSProperties, memo, useState } from 'react';
-import { Flexbox } from 'react-layout-kit';
 
-import { useCdnFn } from '@/ConfigProvider';
-import Img from '@/Img';
-import { LOGO_3D } from '@/Logo/style';
+import Loading from './Loading';
 
 const LIGHT = 'https://gw.alipayobjects.com/os/kitchen/J9jiHITGrs/scene.splinecode';
 const DARK = 'https://gw.alipayobjects.com/os/kitchen/CzQKKvSE8a/scene.splinecode';
 
 export interface LogoSplineProps extends Partial<SplineProps> {
   className?: string;
-  height?: number;
+  height?: number | string;
   style?: CSSProperties;
-  width?: number;
+  width?: number | string;
 }
 
-const LogoSpline = memo<LogoSplineProps>(
-  ({ className, style, width = 640, height = 400, onLoad, ...rest }) => {
-    const genCdnUrl = useCdnFn();
-    const { isDarkMode } = useThemeMode();
-    const [loading, setLoading] = useState(true);
-    return (
-      <Flexbox
-        align={'center'}
-        className={className}
-        flex={'none'}
-        justify={'center'}
-        style={{ height: height, overflow: 'hidden', position: 'relative', width: width, ...style }}
-      >
-        {loading && (
-          <Img
-            alt={'logo'}
-            height={height * 0.5}
-            src={genCdnUrl(LOGO_3D)}
-            style={{ position: 'absolute' }}
-            width={height * 0.5}
-          />
-        )}
-        <Spline
-          onLoad={(splineApp) => {
-            setLoading(false);
-            onLoad?.(splineApp);
-          }}
-          scene={isDarkMode ? DARK : LIGHT}
-          style={{
-            flex: 'none',
-            height: height,
-            width: width,
-          }}
-          {...rest}
-        />
-      </Flexbox>
-    );
-  },
-);
+const LogoSpline = memo<LogoSplineProps>(({ className, style, width, height, onLoad, ...rest }) => {
+  const { isDarkMode } = useThemeMode();
+  const [loading, setLoading] = useState(true);
+  return (
+    <div
+      className={className}
+      style={{ height: height, position: 'relative', width: width, ...style }}
+    >
+      {loading && <Loading />}
+      <Spline
+        onLoad={(splineApp) => {
+          setLoading(false);
+          onLoad?.(splineApp);
+        }}
+        scene={isDarkMode ? DARK : LIGHT}
+        {...rest}
+      />
+    </div>
+  );
+});
 
 export default LogoSpline;
