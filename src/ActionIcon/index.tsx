345780410e52fc239f8032a5de880ff8b0b757fe
sxjeru <sxjeru@gmail.com>
2024-06-07 15:40:11 +0800

    Update index.tsx

--- src/ActionIcon/index.tsx
+++ src/ActionIcon/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { Loader2 } from 'lucide-react';
 import { forwardRef, useMemo } from 'react';
 import { Flexbox, type FlexboxProps } from 'react-layout-kit';
