34ade8d634be590cf5942927375aafb2b1df0801
canisminor1990 <i@canisminor.cc>
2024-07-02 00:56:12 +0800

    🐛 fix: Fix eslint

--- src/Markdown/utils.ts
+++ src/Markdown/utils.ts
@@ -6,7 +6,7 @@ export function escapeDollarNumber(text: string) {
     const nextChar = text[i + 1] || ' ';
 
     if (char === '$' && nextChar >= '0' && nextChar <= '9') {
-      char = String.raw`\$`;
+      char = '\\$';
     }
 
     escapedText += char;
@@ -30,7 +30,5 @@ export function escapeBrackets(text: string) {
 }
 
 export function escapeMhchem(text: string) {
-  return text
-    .replaceAll(String.raw`$\ce{`, String.raw`$\\ce{`)
-    .replaceAll(String.raw`$\pu{`, String.raw`$\\pu{`);
+  return text.replaceAll('$\\ce{', '$\\\\ce{').replaceAll('$\\pu{', '$\\\\pu{');
 }
