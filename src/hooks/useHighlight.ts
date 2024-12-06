090591e56f12b42fc49215f1fca74c49f7c9b968
canisminor1990 <i@canisminor.cc>
2024-12-06 16:53:31 +0800

    🐛 fix: Fix shiki

--- src/hooks/useHighlight.ts
+++ src/hooks/useHighlight.ts
@@ -5,49 +5,22 @@ import {
   transformerNotationHighlight,
   transformerNotationWordHighlight,
 } from '@shikijs/transformers';
-import { type Highlighter, getHighlighter } from 'shiki';
+import { codeToHtml } from 'shiki';
 import useSWR from 'swr';
 
 import { themeConfig } from '@/Highlighter/theme';
 
-import languageMap from './languageMap';
-
 export const FALLBACK_LANG = 'txt';
-
-const FALLBACK_LANGS = [FALLBACK_LANG];
-
-let cacheHighlighter: Highlighter;
-
-const initHighlighter = async (lang: string): Promise<Highlighter> => {
-  let highlighter = cacheHighlighter;
-  const language = lang.toLowerCase();
-
-  if (highlighter && FALLBACK_LANGS.includes(language)) return highlighter;
-
-  if (languageMap.includes(language as any) && !FALLBACK_LANGS.includes(language)) {
-    FALLBACK_LANGS.push(language);
-  }
-
-  highlighter = await getHighlighter({
-    langs: FALLBACK_LANGS,
-    themes: [themeConfig(true), themeConfig(false)],
-  });
-
-  cacheHighlighter = highlighter;
-
-  return highlighter;
-};
 
 export const useHighlight = (text: string, lang: string, isDarkMode: boolean) =>
   useSWR(
-    [lang.toLowerCase(), isDarkMode ? 'dark' : 'light', text].join('-'),
+    [lang.toLowerCase(), isDarkMode ? 'd' : 'l', text].join('-'),
     async () => {
       try {
         const language = lang.toLowerCase();
-        const highlighter = await initHighlighter(language);
-        const html = highlighter?.codeToHtml(text, {
-          lang: languageMap.includes(language as any) ? language : FALLBACK_LANG,
-          theme: isDarkMode ? 'dark' : 'light',
+        const html = await codeToHtml(text, {
+          lang: language,
+          theme: themeConfig(isDarkMode),
           transformers: [
             transformerNotationDiff(),
             transformerNotationHighlight(),
@@ -58,7 +31,7 @@ export const useHighlight = (text: string, lang: string, isDarkMode: boolean) =>
         });
         return html;
       } catch {
-        return '';
+        return text;
       }
     },
     { revalidateOnFocus: false },
