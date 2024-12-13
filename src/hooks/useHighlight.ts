7067207879e3575d69387eb911a0265926a371e7
canisminor1990 <i@canisminor.cc>
2024-12-13 11:17:36 +0800

    🐛 fix: Fix useHighlight

--- src/hooks/useHighlight.ts
+++ src/hooks/useHighlight.ts
@@ -5,8 +5,9 @@ import {
   transformerNotationHighlight,
   transformerNotationWordHighlight,
 } from '@shikijs/transformers';
-import { useLayoutEffect, useState } from 'react';
 import { codeToHtml } from 'shiki';
+import useSWR from 'swr';
+import { Md5 } from 'ts-md5';
 
 import { themeConfig } from '@/Highlighter/theme';
 
@@ -14,37 +15,24 @@ import languageMap from './languageMap';
 
 export const FALLBACK_LANG = 'txt';
 
-const highlight = async (text: string, lang: string, isDarkMode: boolean) => {
-  const language = lang.toLowerCase();
-  return codeToHtml(text, {
-    lang: languageMap.includes(language as any) ? language : FALLBACK_LANG,
-    theme: themeConfig(isDarkMode),
-    transformers: [
-      transformerNotationDiff(),
-      transformerNotationHighlight(),
-      transformerNotationWordHighlight(),
-      transformerNotationFocus(),
-      transformerNotationErrorLevel(),
-    ],
+export const useHighlight = (text: string, lang: string, isDarkMode: boolean) =>
+  useSWR(Md5.hashStr([lang.toLowerCase(), isDarkMode ? 'd' : 'l', text].join('-')), async () => {
+    try {
+      const language = lang.toLowerCase();
+      return codeToHtml(text, {
+        lang: languageMap.includes(language as any) ? language : FALLBACK_LANG,
+        theme: themeConfig(isDarkMode),
+        transformers: [
+          transformerNotationDiff(),
+          transformerNotationHighlight(),
+          transformerNotationWordHighlight(),
+          transformerNotationFocus(),
+          transformerNotationErrorLevel(),
+        ],
+      });
+    } catch {
+      return text;
+    }
   });
-};
-
-export const useHighlight = (text: string, lang: string, isDarkMode: boolean) => {
-  const [data, setData] = useState<string>(text);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useLayoutEffect(() => {
-    setIsLoading(true);
-    highlight(text, lang, isDarkMode).then((date) => {
-      setData(date);
-      setIsLoading(false);
-    });
-  }, [text, lang, isDarkMode]);
-
-  return {
-    data,
-    isLoading,
-  };
-};
 
 export { default as languageMap } from './languageMap';
