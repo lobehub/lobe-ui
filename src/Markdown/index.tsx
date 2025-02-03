91e2341f6378f9819bc3e1dfe9b7fb89288f5765
sxjeru <sxjeru@gmail.com>
2025-02-03 22:26:48 +0800

    Update index.tsx

--- src/Markdown/index.tsx
+++ src/Markdown/index.tsx
@@ -162,7 +162,7 @@ const Markdown = memo<MarkdownProps>(
           ...innerRemarkPluginsAhead,
           remarkGfm,
           enableLatex && remarkMath,
-          isChatMode && remarkBreaks,
+          isChatMode,
           ...innerRemarkPlugins,
         ].filter(Boolean) as any,
       [isChatMode, enableLatex, ...innerRemarkPluginsAhead, ...innerRemarkPlugins],
