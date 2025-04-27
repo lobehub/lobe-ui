4e39ade6ddda5b637f11d7423721bcb3299abaca
canisminor1990 <i@canisminor.cc>
2025-04-27 23:32:36 +0800

    💄 style: Update Segement

--- src/Segmented/style.ts
+++ src/Segmented/style.ts
@@ -4,8 +4,8 @@ export const useStyles = createStyles(({ css, stylish, token }) => {
   return {
     borderless: stylish.variantBorderlessWithoutHover,
     filled: css`
-      background: ${token.colorFillQuaternary};
-      border: 1px solid ${token.colorBorderSecondary};
+      background: ${token.colorBgLayout};
+      border: 1px solid ${token.colorFillQuaternary};
     `,
     glass: stylish.blur,
     outlined: css`
