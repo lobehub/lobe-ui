ccca0376576b473209c9c7a898dc4186b5f3d770
Innei <tukon479@gmail.com>
2026-06-25 15:44:08 +0800

    💄 style: update Tabs active tab highlight background and add box-shadow

--- src/base-ui/Tabs/style.ts
+++ src/base-ui/Tabs/style.ts
@@ -34,7 +34,8 @@ export const styles = createStaticStyles(({ css, cssVar }) => ({
     height: var(--active-tab-height);
     border-radius: ${cssVar.borderRadius};
 
-    background: ${cssVar.colorFillSecondary};
+    background: ${cssVar.colorBgElevated};
+    box-shadow: ${cssVar.boxShadowTertiary};
   `,
   indicatorSquare: css`
     inset-block-end: 0;
